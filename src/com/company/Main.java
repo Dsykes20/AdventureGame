d381474f4d0ee406db1be7112349cefee683473e
denise <d189@umbc.edu>
2022-02-09 11:03:33 -0500

    hhkh

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,8 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("");
+        System.out.println();
+
     }
 }
